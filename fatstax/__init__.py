2100f71a4aa53f5bed498dc399c3b9827b83eefa
Dolphman <therealdolphman@gmail.com>
2016-07-20 09:55:39 -0400

    Added FatStax main object

--- fatstax/__init__.py
+++ fatstax/__init__.py
@@ -1,27 +1,16 @@
-from . import parsing
-from .objects import MasterRow, UpdateRow, ScrappedDataRow
-from .output import outputfile
-from .utils import (sku_update, productimageurl, description,
-					update_master_description, scrappeddata_property)
+from .core.output import outputfile
 
 
 class FatStax(object):
 	"""This class centralizes the parsing, output, and objects
 	functionality of this script"""
 
-	def __init__(self, mastercsv, updatecsv, scrappeddata):
-		#Since I close the file after this, the row must be looped over
-		self.masterrow = list(parsing.parser(mastercsv, MasterRow))
-		self.updaterow = list(parsing.parser(updatecsv, UpdateRow))
-		self.scrappeddata = list(parsing.xlsxparser(scrappeddata, ScrappedDataRow))
+	def __init__(self, mastercsv):
+		#Since I close the file after this, the row must be fully placed into memory
+		self.rows = list(parsing.parser(mastercsv, MasterRow))
 
-	def output(self, master=None, update=None, master_columns=None, update_columns=None):
-		if master:
-			if not master_columns:
-				raise Exception("master_columns must be supplied to output MASTER")
-			outputfile(master, self.masterrow, master_columns)
-		if update:
-			if not update_columns:
-				raise Exception("update_columns must be supplied to output UPDATE")
-			outputfile(update, self.updaterow, update_columns)
 
+	def output(self, master=None, update=None, columns=None, update_columns=None):
+		if not master_columns:
+			raise Exception("master_columns must be supplied to output MASTER")
+		outputfile(master, self.masterrow, master_columns)
