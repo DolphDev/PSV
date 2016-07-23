819550589b56133e36c38b42a35109b251a5a038
Dolphman <therealdolphman@gmail.com>
2016-07-23 13:39:03 -0400

    BaseRow is now a inhertits dict

--- fatstax/core/objects/rowobjects.py
+++ fatstax/core/objects/rowobjects.py
@@ -1,55 +1,54 @@
 from .utils import multireplace
 
-class InnerRow(object):
-    "Represents the data of a objects"
 
-class BaseRow(object):
+class BaseRow(dict):
     """This Base Class represents a row in a spreadsheet"""
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data, *args, **kwargs):
+        super(BaseRow, self).__init__(data)
+        self.construct(*args, **kwargs)
 
-    def __getitem__(self, v):
+    def construct(self, *args, **kwargs):
+        pass
+
+    def getcolumn(self, v):
         "Get column by the orginal column name"
         return self.longcolumn[v]
 
-    def __setitem__(self, a, v):
+    def setcolumn(self, a, v):
         s = multireplace(a.lower(), " ", "(", ")")
-        if s in self.data.keys():
+        if s in self.keys():
             self.__setattr__(s, v)
-        
+        else:
+            raise Exception("Key not valid")
 
-    def __getattr__(self, attr): 
-        """Allows dynamic access to rows
-        This allows algorathic access to rows
-        in a pythonic style"""
-        try:
-            if attr in self.__dict__["data"]:
-                return self.data[attr]["value"]
-        except KeyError:
-            raise AttributeError('\'{}\' has no attribute \'{}\''.format(
-            type(self), attr))
-        raise AttributeError('\'{}\' has no attribute \'{}\''.format(
-            type(self), attr))
+
+    def __getattribute__(self, attr):
+        if attr in super(BaseRow, self).keys():
+            return (self[attr]["value"])
+        else:
+            return super(dict, self).__getattribute__(attr)
 
     def __setattr__(self, attr, v):
         """Allows setting new data to row
         is called if row does not have specially 
         defined set behavior"""
         try:
-            if attr in self.__dict__["data"]:
-                self.data[attr]["value"] = v
+            if hasattr(self, attr):
+                self[attr]["value"] == v
+            else:
+                super(BaseRow, self).__setattr__(attr, v)
+
         except (KeyError, AttributeError):
             super(BaseRow, self).__setattr__(attr, v)
 
-    def setattr(self, attr, v):
-        super(BaseRow, self).__setattr__(attr, v)
 
     @property
     def longcolumn(self):
         """Generates a Dict that uses orginal names of 
         the column, used for output"""
         newdict = {}
-        for k in self.data.keys():
-            newdict.update({self.data[k]["org_name"]: self.data[k]["value"]})
+        for k in self.keys():
+            newdict.update({self[k]["org_name"]: self[k]["value"]})
         return newdict
+
