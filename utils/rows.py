0168ce89192d897f0d110b6810beac90b421ef2c
Dolphman <therealdolphman@gmail.com>
2016-08-02 12:30:06 -0400

    Added Utils for commonly used code

---
+++ utils/rows.py
@@ -0,0 +1,25 @@
+"""
+This file contains rows that provide row objects commonly employed by FatStax
+"""
+import fatstax
+
+class CloudStaxExportRow(fatstax.BaseRow):
+
+    def _subdir(self, max_category):
+        t = "Category {}"
+        for x in range(max_category):
+            try:
+                resp = self.getcolumn(t.format(x+1))
+                if resp:
+                    yield resp
+                else:
+                    yield None
+            except KeyError:
+                yield None
+
+
+    def category_dir(self, max_category):
+        """
+        return list(self._subdir(max_category))
+        """
+        pass
