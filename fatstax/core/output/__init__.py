9ba2bfd76516bdbd32e7247ecc6a7c2141793a81
Dolphman <therealdolphman@gmail.com>
2016-08-02 13:25:03 -0400

    Added Exception to prevent odd bug message.

--- fatstax/core/output/__init__.py
+++ fatstax/core/output/__init__.py
@@ -1,10 +1,11 @@
 import csv
 
 def outputfile(fileloc, rows, columnnames, quote_all=True, encoding="LATIN-1"):
+    if not isinstance(columnnames, list):
+        raise ValueError("Provided Columns must be a list was {}".format(type(columnnames)))
     with open(fileloc, 'w', encoding=encoding, newline='') as csvfile:
         fieldnames = columnnames
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL if quote_all else 0)
-
         writer.writeheader()
         for x in rows:
             writer.writerow(x.longcolumn)
