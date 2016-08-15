572f5e152480317f69e93391f35524bcea9d3427
Dolphman <therealdolphman@gmail.com>
2016-08-15 15:39:17 -0400

    Added Exceptions

---
+++ fatstax/core/exceptions/messages.py
@@ -0,0 +1,11 @@
+
+class RowObjectMsg:
+
+    getformulamsg = "'{}' cell is not a formula."
+    outputrowmsg = "output must be {}, not {}"
+
+class ApiObjectMsg:
+
+    singlefindmsg = "Function returned more than 1 result"
+    getitemmsg = "Row indices must be int, slices, str, tuple, or functions. Not {}"
+    outputmsg = "A ordered list of columns must be supplied to output the file"
