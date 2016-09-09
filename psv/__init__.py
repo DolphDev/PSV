c4d091488b6301e11d652a9f7c2b4d39e4e2793c
Dolphman <therealdolphman@gmail.com>
2016-09-09 09:59:18 -0400

    Small fix for columns names when loading a string

--- psv/__init__.py
+++ psv/__init__.py
@@ -19,13 +19,15 @@ def load(f, cls=BaseRow, outputfile=None, delimiter=",", quotechar='"', mode='r'
         api = Api(data, columns=column_names(f),outputfiled=outputfile, cls=cls, typetranfer=typetranfer)
     return api
 
+
 def loads(csvdoc, columns=None, cls=BaseRow, outputfile=None, delimiter=",", quotechar='"', 
           typetranfer=True, csv_size_max=None):
     if csv_size_max:
         csv_size_limit(csv_size_max)  
     if isinstance(csvdoc, str):
         data = csv.DictReader(csvdoc.split("\n"), delimiter=delimiter, quotechar=quotechar)
-        columns = tuple(next(csv.reader(csvdoc, delimiter=',', quotechar=quotechar)))
+        if not columns:
+            columns = tuple(next(csv.reader(csvdoc, delimiter=',', quotechar=quotechar)))
     else:
         data = csvdoc
     api = Api(data, columns=(columns), outputfiled=outputfile, cls=cls, typetranfer=typetranfer)
