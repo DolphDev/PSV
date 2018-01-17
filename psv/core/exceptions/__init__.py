e849f4acdedefbc91fba5f7f28c4511f1b590ec6
Dolphman <therealdolphman@gmail.com>
2018-01-17 10:34:14 -0500

    Removed Unused Exceptions

--- psv/core/exceptions/__init__.py
+++ psv/core/exceptions/__init__.py
@@ -5,17 +5,6 @@ class PSV_BASE_EXCEPTION(Exception):
     pass
 
 
-class RowError(PSV_BASE_EXCEPTION):
-    pass
-
-
-class FlagError(RowError):
-    pass
-
-
 class SelectionError(PSV_BASE_EXCEPTION):
     pass
 
-
-class ApiError(SelectionError):
-    pass
