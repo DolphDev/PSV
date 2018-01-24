2e7aa44676dcaf453a24d6f0def1c82364afe1b5
Dolphman <therealdolphman@gmail.com>
2018-01-24 14:58:02 -0500

    Setup.py Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Copyright (c) 2016 Joshua Walters
 """
 
 
-version = '0.0.0.1'
+version = '0.1'
 
 from setuptools import setup
 setup(name='psv',
@@ -16,11 +16,13 @@ setup(name='psv',
       url='https://github.com/Dolphman/PSV',
       packages=['psv', 'psv.core', 'psv.core.objects', 'psv.core.parsing', 'psv.core.output', "psv.core.utils", "psv.core.exceptions"],
       package_data={'': ['LICENSE.txt', "readme.md"]},
-      keywords=['csv', "Comma Separated Values", "Python Separated Values"],
+      keywords=['csv', "Comma Separated Values", "Python Separated Values", "psv"],
       classifiers=["License :: OSI Approved :: MIT License",
                    "Operating System :: OS Independent",
                    "Topic :: Utilities",
                    "Programming Language :: Python :: 3.2",
                    "Programming Language :: Python :: 3.3",
                    "Programming Language :: Python :: 3.4",
-                   "Programming Language :: Python :: 3.5"]      )
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6"]
+                    )
