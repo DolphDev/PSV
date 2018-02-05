a71a6fde0c005a294b23a41fc4205f10d31e2e1b
DolphDev <therealdolphman@gmail.com>
2018-02-05 15:16:59 -0500

    Moved to a 2 decimal versionally system

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ Copyright (c) 2016 Joshua Walters
 """
 
 
-version = '0.1'
+version = '0.1.0'
 
 from setuptools import setup
 setup(name='psv',
       install_requires=["tabulate"],
       version=version,
       description='CSV Parser',
-      author='Joshua Walters',
+      author='Joshua W',
       author_email='therealdolphman@gmail.com',
       url='https://github.com/Dolphman/PSV',
       packages=['psv', 'psv.core', 'psv.core.objects', 'psv.core.parsing', 'psv.core.output', "psv.core.utils", "psv.core.exceptions"],
