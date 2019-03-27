53e1481d8eb142a208952761e7695852b9012943
DolphDev <dolphdevgithub@gmail.com>
2019-03-27 09:51:01 -0400

    Removed Tabs

--- psv/__init__.py
+++ psv/__init__.py
@@ -1,5 +1,5 @@
 from .main import (load, loads, loaddir, new, safe_load, opencsv,
-				   csv_size_limit, column_names, column_names_str)
+                   csv_size_limit, column_names, column_names_str)
 from .core.objects import banned_columns
 from .core.utils.objects import ROW_OBJ
 
